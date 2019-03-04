e25c390cdbc4603a9ae03fbf19a9fe4b010b09e3
Mark May <mamark90@gmail.com>
2019-03-04 16:34:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,9 @@
 def countdown(number)
   zero_num = 0
   while number > zero_num
+    def countdown_with_sleep(number_2)
+      sleep number_2
+    end
   puts "#{number} SECOND(S)!"
   number -= 1
   end

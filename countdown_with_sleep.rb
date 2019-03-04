414dc36e01815da9d32ce29b5d2a5a436d142aa1
Mark May <mamark90@gmail.com>
2019-03-04 15:57:42 +0000

    Automatically backed up by Learn

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -1,12 +1,12 @@
 #write your code here
 
 
-def countdown(number)
+def countdown_with_sleep(number)
   zero_num = 0
   while number > zero_num
   puts "#{number} SECOND(S)!"
   number -= 1
-  sleep 20
+  sleep (number)
   end
 puts "HAPPY NEW YEAR!"
 end
